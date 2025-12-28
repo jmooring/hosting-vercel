65938e4258bdc7e0b898c22941f22cbed56dfb51
Joe Mooring <joe.mooring@veriphor.com>
2025-12-28 12:37:20 -0800

    Update deps

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ This site:
 1. Transpiles Sass to CSS using Dart Sass
 1. Performs vendor prefixing of CSS rules using the postcss, postcss-cli, and autoprefixer Node.js packages
 1. Processes CSS files using the tailwindcss and @tailwindcss-cli Node.js packages
-1. Encodes images to the WebP format to verify that we're using Hugo's extended edition
+1. Encodes images to the WebP format
 1. Includes a content file named hugö.md to verify that the Git `core.quotepath` setting is `false` [^1]
 
 [^1]: See [issue #9810](https://github.com/gohugoio/hugo/issues/9810). Git's `core.quotepath` setting is `false` if `/tests/hugö` has a non-zero "last modified" date.
--- build.sh
+++ build.sh
@@ -9,10 +9,10 @@
 
 main() {
 
-  DART_SASS_VERSION=1.93.3
-  GO_VERSION=1.25.4
-  HUGO_VERSION=0.152.2
-  NODE_VERSION=24.11.0
+  DART_SASS_VERSION=1.97.1
+  GO_VERSION=1.25.5
+  HUGO_VERSION=0.153.3
+  NODE_VERSION=24.12.0
 
   export TZ=Europe/Oslo
 
@@ -32,10 +32,10 @@ main() {
 
   # Install Hugo
   echo "Installing Hugo ${HUGO_VERSION}..."
-  curl -sLJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  curl -sLJO "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
   mkdir "${HOME}/.local/hugo"
-  tar -C "${HOME}/.local/hugo" -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
-  rm "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+  tar -C "${HOME}/.local/hugo" -xf "hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
+  rm "hugo_${HUGO_VERSION}_linux-amd64.tar.gz"
   export PATH="${HOME}/.local/hugo:${PATH}"
 
   # Install Node.js
